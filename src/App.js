d50893d90d31bdc92c5099940f3c27288f448c37
Mirza Buğra Özçelik <ozcelik.mirzabugra@gmail.com>
2024-09-14 22:18:14 +0300

    component render effect with using useState hook

--- src/App.js
+++ src/App.js
@@ -4,25 +4,37 @@ import './App.css';
 function App() {
 
   // const [lastName, setLastName] = useState('Ozc')
-  const [firstName, setFirstName] = useState('Mirz');
-  const [names, setNames] = useState(['A', 'B', 'C', 'D'])
-  const [userInfo, setUserInfo] = useState({username: "mirz", password: "1212"})
-  const [show, setShow] = useState(false);
-
-
-  console.log(names);
-
+  // const [firstName, setFirstName] = useState('Mirz');
+  // const [names, setNames] = useState(['A', 'B', 'C', 'D'])
+  // const [userInfo, setUserInfo] = useState({username: "mirz", password: "1212"})
+  // const [show, setShow] = useState(false);
+  // console.log(names);
   // const handleChange = () => {
   //   setFirstName("Mirzorts");
   // }
 
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count+1)
+  }
+
+  console.log('component render edildi yani function App tekrardan calisti')
+
   return (
+    // <div>
+    //   {names.map((name, index) => {
+    //     <div key={index}>{name}</div>
+    //   })}
+    //   <div>
+    //     {userInfo.username} {userInfo.password}
+    //   </div>
+    // </div>
+
     <div>
-      {names.map((name, index) => {
-        <div key={index}>{name}</div>
-      })}
+        {count}
       <div>
-        {userInfo.username} {userInfo.password}
+        <button onClick={increase}>Increase</button>
       </div>
     </div>
   );
