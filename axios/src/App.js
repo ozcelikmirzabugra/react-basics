2fe7cb97e80628fea9cc8aafab1496f50c95522f
Mirza Buğra Özçelik <ozcelik.mirzabugra@gmail.com>
2024-09-15 15:04:27 +0300

    getting request with two different url

--- axios/src/App.js
+++ axios/src/App.js
@@ -12,38 +12,50 @@ function App() {
     console.log(response.data)
   }
 
-  const getUserById = async (userId) => {
-    const response = await axios.get(BASE_URL+'/users/'+userId);
-    console.log(response.data)
-  }
-
+  
   const createUser = async (newUser) => {
     const response = await axios.post(`${BASE_URL}/users`, newUser);
     console.log(response.data)
   }
-
+  
   const updateUser = async (userId, updatedUser) => {
-    const response = await axios.put(`${BASE_URL}/users/${userId}`, updatedUser);
+    await axios.put(`${BASE_URL}/users/${userId}`, updatedUser);
   }
-
+  
   const deleteUserById = async (userId) => {
     const deletedResponse = await axios.delete(`${BASE_URL}/users/${userId}`)
     console.log(deletedResponse.data)
   }
+  
+  const getUserById = async (userId) => {
+    const response = await axios.get(BASE_URL+'/users/'+userId);
+    return response.data;
+  }
+
+  const getPostById = async (postId) => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/" + postId);
+    return response.data;
+  }
+
+  const getPost = async () => {
+    const postId = await getUserById(1);
+    const postData = await getPostById(postId);
+    console.log(postData)
+  }
 
   useEffect(()=>{
-    // getAllUsers();
-    // getUserById(1);
-    // const newUser = {
-    //   "username": "mirzort",
-    //   "password": "123"
-    // }
-    // createUser(newUser);
+    getAllUsers();
+    getUserById(1);
+    const newUser = {
+      "username": "mirzort",
+      "password": "123"
+    }
+    createUser(newUser);
 
-    // updateUser('2', {
-    //   'username': 'mirzasu',
-    //   'password': '321'
-    // })    
+    updateUser('2', {
+      'username': 'mirzasu',
+      'password': '321'
+    })    
 
     deleteUserById('2');
   },[])
