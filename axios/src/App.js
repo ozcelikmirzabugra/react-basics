e1023835ea7642ff1b9edeb55e2bf71c9eaf5302
Mirza Buğra Özçelik <ozcelik.mirzabugra@gmail.com>
2024-09-15 14:29:06 +0300

    get and post request by using axios on react framework

---
+++ axios/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:3005";
+
+function App() {
+
+
+  const getAllUsers = async () => {
+    const response = await axios.get(BASE_URL+'/users');
+    console.log(response.data)
+  }
+
+  const getUserById = async (userId) => {
+    const response = await axios.get(BASE_URL+'/users/'+userId);
+    console.log(response.data)
+  }
+
+  const createUser = async (newUser) => {
+    const response = await axios.post(`${BASE_URL}/users`, newUser);
+    console.log(response.data)
+  }
+
+  useEffect(()=>{
+    getAllUsers();
+    // getUserById(1);
+    const newUser = {
+      "username": "mirzort",
+      "password": "123"
+    }
+    createUser(newUser);
+  },[])
+
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
